aba712ef15fa9ff54e372bf6475b6330f17a6e9e
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-01-27 19:49:10 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,34 +1,15 @@
-- [中文](#discord机器人框架模块模板)
-- [English](#discord-bot-framework-module-template)
-
-# Discord机器人框架模块模板
-这是针对[Discord机器人内核](https://github.com/retr0-init/Discord-Bot-Framework-Kernel.git)配套的模块模板。其开发可以参考[interactions.py](https://interactions-py.github.io/interactions.py/)。
-
-## 开发中需要考虑的事
-- 模块主要点在[`main.py`](main.py)中。
-- 所有主要的工作都应该在继承自`interactions.Extension`的类中进行。
-- 最好在指令基或指令组中定义你的命令。
-- 你可以用另一个`.py`文件来定义内部模块函数。这个文件需要与`main.py`同级。注意你要像以下来引入内部模块：
-```python
-from . import xxx # xxx是内部模块的名字
+# Discord自动踢人模块
+1. 在内核中加载后，用以下命令来初始化模块。第一个参数为消息数量阀值，可忽略，默认值为10。第二个参数为天数阀值，可忽略，默认值为30。
 ```
-- 在[`requirements.txt`](requirements.txt)中写入模块所需的第三方库。
-- 在[`CHANGELOG`](CHANGELOG)中更新改动。
-- 在机器人内核文件夹以外的文件访问会被拒绝。
-- 外部程序执行会被拒绝。
-
-# Discord-Bot-Framework-Module-Template
-This is the module template for [Discord-Bot-Framework-Kernel](https://github.com/retr0-init/Discord-Bot-Framework-Kernel.git). The development refers to [interactions.py](https://interactions-py.github.io/interactions.py/).
-
-## Things to consider for development
-- The module's entry point is in [`main.py`](main.py).
-- Do all the main work in the class inherited from `interactions.Extension`.
-- It's better to define your commands under either a command base or group.
-- You can use another `.py` file for internal module under the same directory as `main.py`. Be aware that you need to import it like
-```python
-from . import xxx # xxx is the internal module script name
+/autokick setup 10 30
 ```
-- Put python module requirements in [`requirements.txt`](requirements.txt). Do NOT delete this file.
-- Update your changes in [`CHANGELOG`](CHANGELOG).
-- The file access to the files other than the kernel directory will be denied.
-- The external program execution will be denied.
+2. 当初始化完成后，用`/autokick start`开启功能。
+3. 如果想停止这个系统，用`/autokick stop`指令。
+
+# Discord AutoKick Module
+1. After being loaded in the kernel, setup the function with the command below. The first optional parameter is the message count threshold default to 10. The second optional one is the day count threshold default to 30.
+```
+/autokick setup 10 30
+```
+2. After the setup is completed, use `/autokick start` to start the function.
+3. If you want to stop the system, use `/autokick stop`.
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 '''
-Discord-Bot-Module template. For detailed usages,
- check https://interactions-py.github.io/interactions.py/
+Automatically kick members who do not have more than n messages within m days.
 
 Copyright (C) 2024  __retr0.init__
 
@@ -18,40 +17,165 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 import interactions
-# Use the following method to import the internal module in the current same directory
-from . import internal_t
+from interactions.api.events import MemberRemove, MessageCreate
+from collections import deque
+import asyncio
+import datetime
+from config import DEV_GUILD
 
 '''
-Replace the ModuleName with any name you'd like
+Autokick module
+Base: autokick
 '''
-class ModuleName(interactions.Extension):
+class ExtRetr0initAutokick(interactions.Extension):
     module_base: interactions.SlashCommand = interactions.SlashCommand(
-        name="replace_your_command_base_here",
-        description="Replace here for the base command descriptions"
-    )
-    module_group: interactions.SlashCommand = self.module_base.group(
-        name="replace_your_command_group_here",
-        description="Replace here for the group command descriptions"
+        name="autokick",
+        description="AutoKick System to automatically kick members"
     )
 
-    @self.module_group.subcommand("ping", sub_cmd_description="Replace the description of this command")
+    initialised: bool = False
+    started: bool = False
+
+    ''' All members
+    {
+        <member id> : [message],
+    }
+    '''
+    all_members: dict[int, deque[interactions.Message]] = {}
+
+    ignored_roles: list[int] = []
+
+    threshold_message: int = 10
+    threshold_days: int = 30
+
+    reference_time: datetime.datetime = datetime.datetime(1970, 1, 1)
+
+    @self.module_base.subcommand("setup", sub_cmd_description="Setup the autokick feature and generate statistics")
+    @interactions.check(interactions.is_owner())
     @interactions.slash_option(
-        name = "option_name",
-        description = "Option description",
-        required = True,
-        opt_type = interactions.OptionType.STRING
+        name = "th_message",
+        description = "Message threshold. Default to 10.",
+        required = False,
+        opt_type = interactions.OptionType.INTEGER,
+        min_value = 2
     )
-    async def module_group_ping(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
+    @interactions.slash_option(
+        name = "th_days",
+        description = "Threshold of calculation durations. Default to 30.",
+        required = False,
+        opt_type = interactions.OptionType.INTEGER,
+        min_value = 1
+    )
+    async def command_setup(self, ctx: interactions.SlashContext, th_message: int = 10, th_days: int = 30):
+        if self.kick_task.started:
+            self.kick_task.stop()
+        self.started = False
+        self.initialised = False
+        now: interactions.Timestamp = interactions.Timestamp.now()
+        await ctx.defer()
+        self.threshold_message = th_message
+        self.threshold_days = th_days
+        self.all_members = {
+            mem.id: deque([])
+            for mem in ctx.guild.members
+        }
+        all_channels: list = ctx.guild.channels
+        # Get all message in the guild within the day threshold
+        for channel in all_channels:
+            async for message in channel.history(limit=0):
+                if now - message.timestamp > datetime.timedelta(days=th_days):
+                    continue
+                for member in all_members.keys():
+                    if message.author.id == member:
+                        all_members[member].append(message)
+                        break
+        # Sort the message_id's according to the sent timestamp
+        for member in self.all_members.keys():
+            self.all_members[member] = deque(sorted(self.all_members[member], key=lambda m: m.timestamp))
+        await ctx.send(f"Setup complete! The member who does not send more than {self.threshold_message} messages in {self.threshold_days} days will be kicked.")
+        self.reference_time = now
+        self.initialised = True
         internal_t.internal_t_testfunc()
 
-    @self.module_base.subcommand("pong", sub_cmd_description="Replace the description of this command")
+    async def kick_member(self, user: int):
+        u: interactions.Member = await self.bot.fetch_member(user_id=user, guild_id=DEV_GUILD)
+        if u is not None:
+            dm_channel: interactions.DMChannel = await u.fetch_dm()
+            if dm_channel is not None:
+                dm_channel.send(f"您好。由于您在{self.threshold_days}天内在{ctx.guild.name}的发言不足{self.threshold_message}条。根据服务器规则，将把您踢出该服务器。如果您想重返本服务器的话，请重新加入。在此感谢您的理解与支持。祝一切安好。")
+            u.kick(reason=f"From {self.reference_time - datetime.timedelta(days=30)} to {self.reference_time} has less than {self.threshold_message} messages")
+            await asyncio.sleep(5)
+
+    @interactions.Task.create(interactions.IntervalTrigger(hours=12))
+    async def kick_task(self):
+        # Start flag guard
+        if not self.started:
+            return
+        self.reference_time = interactions.Timestamp.now()
+        td: datetime.timedelta = datetime.timedelta(days=self.threshold_days)
+        for member in self.all_members.keys():
+            # Remove the old messages according to current reference time
+            for message in list(self.all_members[member]):
+                if self.reference_time - message.timestamp <= td:
+                    break
+                else:
+                    self.all_members[member].popleft()
+            member_obj: interactions.Member = await self.bot.fetch_member(member)
+            if any(map(member_obj.has_role, self.ignored_roles)):
+                continue
+            if self.reference_time - member_obj.joined_at < datetime.timedelta(days=self.threshold_days):
+                continue
+            if len(self.all_members[member]) < self.threshold_message:
+                await self.kick_member(member)
+
+    @self.module_base.subcommand("start", sub_cmd_description="Start the AutoKick system")
+    @interactions.check(interactions.is_owner())
     @interactions.slash_option(
-        name = "option_name",
-        description = "Option description",
-        required = True,
-        opt_type = interactions.OptionType.STRING
+        name = "force",
+        description = "Force start without role setup",
+        required = False,
+        opt_type = interactions.OptionType.BOOLEAN
     )
-    async def module_group_pong(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
-        internal_t.internal_t_testfunc()
+    async def command_start(self, ctx: interactions.SlashContext, force: bool = False):
+        if not self.initialised:
+            await ctx.send("Please use the `setup` command at first!", ephemeral=True)
+            return
+        if not force and len(self.ignored_roles) == 0:
+            await ctx.send("There is no ignored role configured! Please run `setup_roles` command to set it. If you want to continue, please set `force` parameter to `True`.")
+            return
+        kicked_members: dict[int, int] = {
+            mem: len(self.all_members[mem])
+            for mem  in self.all_members.keys()
+            if len(self.all_members[mem]) < self.threshold_message
+        }
+        self.kick_task.start()
+        self.started = True
+        await ctx.send("AutoKick system started")
+
+    @self.module_base.subcommand("stop", sub_cmd_description="Stop the Autokick task")
+    @interactions.check(interactions.is_owner())
+    async def command_stop(self, ctx: interactions.SlashContext):
+        if self.kick_task.started:
+            self.kick_task.stop()
+            self.started = False
+            await ctx.send("Autokick System stopped.")
+        else:
+            await ctx.send("Autokick System has not been started yet")
+
+    @interactions.listen(MemberRemove)
+    async def on_memberremove(self, event: MemberRemove):
+        '''
+        When the member is deleted from the server, remove the user from all_users dictionary
+        '''
+        if self.initialised:
+            member_obj: interactions.Member = event.member
+            if member_obj.id in self.all_members:
+                self.all_members.pop(member_obj.id)
+
+    @interactions.listen(MessageCreate)
+    async def on_messagecreate(self, event: MessageCreate):
+        '''
+        Prepend message to the list
+        '''
+        if self.initialised:
+            self.all_members[event.message.author.id].append(event.message)
